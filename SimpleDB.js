a444ef6f3015ed07fb5f74ea6c7dcb3b10866a9d
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-17 18:20:34 -0700

    three passing tests!

--- SimpleDB.js
+++ SimpleDB.js
@@ -1,16 +1,41 @@
-// import { readFile, writeFile } from 'fs/promises';
+import { writeFile, readFile } from 'fs/promises';
 import path from 'path';
 import shortid from 'shortid';
 
 export class SimpleDB {
-  constructor(rootDir) {
-    const fileName = `${shortid.generate()}.txt`;
-    this.newFile = path.join(rootDir, fileName);
+  constructor(instance) {
+    this.storeDest = instance;
   }
 
-  //save then serialize into json object
+// method that saves object
+  // save method takes in an object. generate Id & assign id to object Id..
+  // create JSON file name with object id.
+  // create path by joining location with the file name
+  // stringify object
+  // return written file of the path that is stringified.
 
-  //get = parse
+  getPath(id){
+    const fileName = `${id}.json`;
+    const filePath = path.join(this.storeDest, fileName);
+    return filePath;
+  }
+  save(obj) {
+    obj.id = shortid.generate();
+    const filePath = this.getPath(obj.id);
+    const stringified = JSON.stringify(obj);
+    return writeFile(filePath, stringified);
+  } 
 
-//getAll
+  get(id) {
+    const getPathById = this.getPath(id);
+    return readFile(getPathById, 'utf-8').then(contentTxt => 
+      JSON.parse(contentTxt)).catch((err) => {
+      if (err.code === 'ENOENT') {
+        return null;
+      }
+      throw err;
+    });
+    // const parsedObject = JSON.parse(getPathById);
+    // return parsedObject;
+  }
 }
--- __tests__/simpleDB.test.js
+++ __tests__/simpleDB.test.js
@@ -3,16 +3,16 @@ import { SimpleDB } from '../SimpleDB.js';
 
 
 describe('file copier', () => {
-  const destination = './__tests__/destination';
+  const storeDest = '../store';
 
   beforeEach(() => {
-    return rm(destination, { force: true, recursive: true }).then(() => {
-      return mkdir(destination);
+    return rm(storeDest, { force: true, recursive: true }).then(() => {
+      return mkdir(storeDest);
     });
   });
 
   it('should have an id', () => {
-    const db = new SimpleDB(destination);
+    const db = new SimpleDB(storeDest);
     const mabel = {
       name: 'mabel',
       age:5
@@ -22,32 +22,29 @@ describe('file copier', () => {
       expect(mabel.id).toEqual(expect.any(String)); 
     });
   });
-});
 
-it('should save and retrieve an object', () => {
-  const savedInstance = new SavedObject(destination);
-  const getInstance = new GetObject(id);
-  const mabel = {
-    name: 'mabel',
-    age:5
-  };
+  it('should save and retrieve an object', () => {
+    const savedInstance = new SimpleDB(storeDest);
+    const mabel = {
+      name: 'mabel',
+      age:5
+    };
 
-  return savedInstance
-    .save(mabel)
-    .then(() => {
-      return getInstance.get(id);
-    })
-    .then((booger) => {
-    //mabel.id?
-      expect(booger).toEqual(mabel.id);
-    });
-});
-
-it('should return null if no object was returned', () => {
-  const getInstance = new GetObject(id);
-
-  return getInstance.get().then((booger) => {
-    expect(booger).toBeNull();
+    return savedInstance
+      .save(mabel)
+      .then(() => {
+        return savedInstance.get(mabel.id);
+      })
+      .then((booger) => {
+        expect(booger).toEqual(mabel);
+      });
   });
-});
-       
+
+  it('should return null if no object was returned', () => {
+    const getInstance = new SimpleDB(storeDest);
+
+    return getInstance.get().then((booger) => {
+      expect(booger).toBeNull();
+    });
+  });
+});       
